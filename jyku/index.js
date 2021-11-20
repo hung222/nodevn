db9b183b922599e34554bc1eb03fab2641edb57f
hung222 <90371118+hung222@users.noreply.github.com>
2021-11-20 13:54:21 +0700

    Update index.js

--- jyku/index.js
+++ jyku/index.js
@@ -4,8 +4,7 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('alibabababa');
 });
-
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000,function (){
+console.log("listwning on port');
 });
+
