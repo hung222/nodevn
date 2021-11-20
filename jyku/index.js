d38394121a45fc80a59e24c75c6ec010b7d778ea
hung222 <90371118+hung222@users.noreply.github.com>
2021-11-20 12:00:27 +0700

    Update index.js

--- jyku/index.js
+++ jyku/index.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
 });
 
 // use port 3000 unless there exists a preconfigured port
-var port = process.env.port || 3000;
+const port = process . env . PORT || 8001 ;
 
 app.listen(port);
