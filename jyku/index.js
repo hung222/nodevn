e5499d0f7c6d26891970f63e69cc1003595a4665
hung222 <90371118+hung222@users.noreply.github.com>
2021-11-20 18:37:21 +0700

    Update index.js

--- jyku/index.js
+++ jyku/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('alibabababa');
+
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
-
-
