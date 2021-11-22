581daa834a29e0bbd8088f2011ccad4b6899dab4
thienphuc-dev <62225265+thienphuc-dev@users.noreply.github.com>
2021-11-22 16:35:17 +0700

    Update index.js

--- jyku/index.js
+++ jyku/index.js
@@ -1,8 +1,8 @@
+const express = require("express")
+const app = express()
 
 
-
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+const server_port = process.env.PORT || 3000;
+app.listen(server_port, function() {
+    console.log('Listening on port ', server_port);
 });
