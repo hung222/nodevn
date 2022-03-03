173fc64ecb442d5a9a6309f2988a7608817e5031
hung222 <90371118+hung222@users.noreply.github.com>
2022-03-03 16:18:07 +0700

    Update index.js

--- tlw/index.js
+++ tlw/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send("qhtdlpchqaqlcnbubvbc");
+   res.send("2bvltdoaltdlhqaltd");
 });
 
 app.listen(process.env.PORT || 5000)
