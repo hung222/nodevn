6d15bf57df86855f945ece577cbd6618b1c6b6cd
hung222 <90371118+hung222@users.noreply.github.com>
2022-06-17 16:21:15 +0700

    Delete tlw directory

--- tlw/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res){
-   res.send("chào bạn anh tuấn làm nodejs chung không hihihihihihi");
-});
-
-app.listen(process.env.PORT ||5000)
