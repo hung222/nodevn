ba1ee0eda48a375e7ccf7b6e79c14940f263691f
hung222 <90371118+hung222@users.noreply.github.com>
2022-06-17 21:17:00 +0700

    Create index.js

---
+++ kicuvu/index.js
@@ -0,0 +1,10 @@
+var expres = require("express");
+var app = express();
+app set("view engine","ejs");
+app set("views","/.views");
+app.listen(process.env.PORT || 3000);
+
+app.get("/",function (req ,res){
+      res.render("trang chu");
+});
+ 
