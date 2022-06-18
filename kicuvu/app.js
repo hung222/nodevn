3334c5e0e52ec25e5ee7e8d0b392811dc0d5d630
hung222 <90371118+hung222@users.noreply.github.com>
2022-06-18 10:32:55 +0700

    Update app.js

--- kicuvu/app.js
+++ kicuvu/app.js
@@ -101,3 +101,23 @@ app.get('/', validate, function (req, res) {
 });
 
 // ...
+var cacheView = function(req, res, next) {
+  var view_key = '_view_cache_' + req.originalUrl || req.url;
+  mc.get(view_key, function(err, val) {
+    if(err == null && val != null) {
+      // Found the rendered view -> send it immediately
+      res.send(val.toString('utf8'));
+      return;
+    }
+    // Cache the rendered view for future requests
+    res.sendRes = res.send
+    res.send = function(body){
+      mc.set(view_key, body, {expires:0}, function(err, val){/* handle error */})
+      res.sendRes(body);
+    }
+    next();
+  });
+}
+
+app.get('/', validate, cacheView, function (req, res) {
+  // ...
