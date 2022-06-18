bf1d14b7e65dcac849a701da7f4703eb0b7d6075
hung222 <90371118+hung222@users.noreply.github.com>
2022-06-18 07:34:27 +0700

    Update index.js

--- kicuvu/index.js
+++ kicuvu/index.js
@@ -1,10 +1,10 @@
 var expres = require("express")
 var app = express();
-app set = ("view engine" , "ejs" );
-app set = ("views", ". /views");
+app set("view engine","ejs");
+app set("views","./views");
 app.listen(process.env.PORT || 3000);
 
 app.get("/",function (req ,res){
-      res.render("trang chủ web");
+      res.render("trang chủ");
 });
  
